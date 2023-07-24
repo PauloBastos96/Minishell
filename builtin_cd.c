dff4c92553b39821b2d5447fa9241dec7db74707
Paulo Andre Rodrigues De Bastos <paulorod@c2r12s6.42lisboa.com>
2023-07-24 16:25:17 +0100

    cd command now supports changing to HOME directory using '~'

--- builtin_cd.c
+++ builtin_cd.c
@@ -6,19 +6,38 @@
 /*   By: paulorod <paulorod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 15:49:13 by paulorod          #+#    #+#             */
-/*   Updated: 2023/07/21 16:22:06 by paulorod         ###   ########.fr       */
+/*   Updated: 2023/07/24 13:24:48 by paulorod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*Returns the home path when using '~'*/
+static char	*get_home_path(char *path)
+{
+	char	*home_path;
+	char	*tmp;
+
+	home_path = getenv("HOME");
+	tmp = ft_strtrim(path, "~");
+	free(path);
+	path = ft_strjoin(home_path, tmp);
+	free(tmp);
+	return (path);
+}
+
 /*Builtin cd command*/
 void	ft_cd(char *command)
 {
 	char	*path;
+	bool	can_free;
 
+	can_free = true;
 	path = ft_strtrim(command, "cd ");
+	if (ft_strncmp(path, "~", 1) == 0)
+		path = get_home_path(path);
 	if (chdir(path) == -1)
 		perror(NULL);
-	free(path);
+	if (can_free)
+		free(path);
 }
