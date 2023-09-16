3899d015cf0fd80c95aed31e7041b45e57c22c86
Francisco-Zeferino <franciscozeferino06@gmail.com>
2023-09-16 19:09:23 +0100

    Heredoc expansions almost done!

--- srcs/redirections/heredoc.c
+++ srcs/redirections/heredoc.c
@@ -6,47 +6,86 @@
 /*   By: ffilipe- <ffilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 16:10:51 by ffilipe-          #+#    #+#             */
-/*   Updated: 2023/09/15 16:07:41 by ffilipe-         ###   ########.fr       */
+/*   Updated: 2023/09/16 19:06:35 by ffilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-char* expand_env_var(t_shell *shell,char *var)
+char	*str_replace(char *string, const char *substr, const char *replacement)
 {
-	return(ft_getenv(var, &shell->env));
-}
+	char	*tok;
+	char	*newstr;
+	char	*oldstr;
+	int		oldstr_len;
+	int		substr_len;
+	int		replacement_len;
 
-char* str_replace(char* string, const char* substr, const char* replacement) {
-	char* tok = NULL;
-	char* newstr = NULL;
-	char* oldstr = NULL;
-	int   oldstr_len = 0;
-	int   substr_len = 0;
-	int   replacement_len = 0;
-
+	tok = NULL;
+	newstr = NULL;
+	oldstr = NULL;
+	oldstr_len = 0;
+	substr_len = 0;
+	replacement_len = 0;
 	newstr = ft_strdup(string);
 	substr_len = ft_strlen(substr);
 	replacement_len = ft_strlen(replacement);
-	if (substr == NULL || replacement == NULL) {
-		return newstr;
-	}
-	while ((tok = ft_strnstr(newstr, substr, ft_strlen(newstr)))) {
+	if (substr == NULL || replacement == NULL)
+		return (newstr);
+	while ((tok = ft_strnstr(newstr, substr, ft_strlen(newstr))))
+	{
 		oldstr = newstr;
 		oldstr_len = ft_strlen(oldstr);
-		newstr = (char*)malloc(sizeof(char) * (oldstr_len - substr_len + replacement_len + 1));
-		if (newstr == NULL) {
-			free(oldstr);
-			return NULL;
-		}
+		newstr = (char *)malloc(sizeof(char) * (oldstr_len - substr_len
+				+ replacement_len + 1));
+		if (newstr == NULL)
+			return (free(oldstr), NULL);
 		ft_memcpy(newstr, oldstr, tok - oldstr);
 		ft_memcpy(newstr + (tok - oldstr), replacement, replacement_len);
-		ft_memcpy(newstr + (tok - oldstr) + replacement_len, tok + substr_len, oldstr_len - substr_len - (tok - oldstr));
+		ft_memcpy(newstr + (tok - oldstr) + replacement_len, tok + substr_len,
+			oldstr_len - substr_len - (tok - oldstr));
 		ft_memset(newstr + oldstr_len - substr_len + replacement_len, 0, 1);
 		free(oldstr);
 	}
-	free(string);
-	return newstr;
+	return (free(string), newstr);
+}
+
+char	*get_var(char *str)
+{
+	int		i;
+	int		j;
+	char	*var;
+
+	i = 0;
+	j = 0;
+	while (str[i] && str[i] != '$')
+		i++;
+	if (str[i] == '$')
+	{
+		while (str[i] && str[i] != ' ' && str[i] != '\'' && str[i] != '\"')
+		{
+			i++;
+			j++;
+		}
+		var = ft_substr(str, i - j, j);
+		return (var);
+	}
+	return (NULL);
+}
+
+char	*set_expansion(t_shell *shell, char *str)
+{
+	char	*var;
+	char	*expanded_var;
+
+	if (get_var(str))
+	{
+		var = get_var(str);
+		if(ft_getenv(var + 1, &shell->env))
+			expanded_var = ft_getenv(var + 1, &shell->env);
+		str = str_replace(str, var, expanded_var);
+	}
+	return (str);
 }
 
 /*Get heredoc error message*/
@@ -67,7 +106,6 @@ void	handle_redir_hdoc(t_shell *shell)
 {
 	t_cmd	*cmd;
 	char	*definer;
-	char	*expanded_var;
 	int		h_doc[2];
 
 	cmd = shell->cmd;
@@ -84,7 +122,7 @@ void	handle_redir_hdoc(t_shell *shell)
 		}
 		if (ft_strcmp(definer, cmd->redirs->redirection) == 0)
 			break ;
-		definer = str_replace(definer, "$PWD", expanded_var);
+		definer = set_expansion(shell, definer);
 		write(h_doc[1], definer, ft_strlen(definer));
 		write(h_doc[1], "\n", 1);
 	}
